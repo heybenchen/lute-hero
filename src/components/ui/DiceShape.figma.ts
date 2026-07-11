b05acdfb72c04ba6d32bf98160a9f0d92f960d94
Ben Chen <shuyi.chen@figma.com>
2026-07-11 15:16:10 -0400

    Add right padding to genre select dropdowns

---
+++ src/components/ui/DiceShape.figma.ts
@@ -0,0 +1,27 @@
+export default {
+  "codeProperties": {
+    "type": {
+      "type": "string",
+      "label": "Type",
+      "control": "select",
+      "options": [
+        {
+          "value": "d4",
+          "label": "D4"
+        },
+        {
+          "value": "d6",
+          "label": "D6"
+        },
+        {
+          "value": "d12",
+          "label": "D12"
+        },
+        {
+          "value": "d20",
+          "label": "D20"
+        }
+      ]
+    }
+  }
+}
